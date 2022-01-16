7bca5526695bac579cdb98b6d379beda50aeb362
he_yuwei <he20010515@163.com>
2022-01-17 00:11:40 +0800

    linearequations update

--- src/linearequations.c
+++ src/linearequations.c
@@ -4,3 +4,24 @@ char *test(void)
 {
     printf("test function from linearquation");
 }
+
+Matrix *linear_solve(Matrix *A, Matrix *b)
+{
+    if (A->col_size != A->row_size) // 是否是矩阵
+    {
+        return -1;
+    }
+    if (A->row_size != b->row_size) //A b 是否匹配
+    {
+        return -1;
+    }
+    if (b->col_size != 1) // b 是否为列向量
+    {
+        return -1;
+    }
+    Matrix *x = matrix_alloc(b->row_size, 1);
+    Matrix *temp = matrix_alloc(b->row_size, 1);
+
+    matrix_free(temp);
+    return x;
+}
