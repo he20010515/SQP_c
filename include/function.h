4f8ec2c0644699170bb6c3d6decbdf80dce6a00f
he_yuwei <he20010515@163.com>
2022-04-25 11:01:44 +0800

    添加了function与indexset头文件的注释

--- include/function.h
+++ include/function.h
@@ -1,3 +1,13 @@
+/*
+ * @Author: HeYuwei
+ * @Date: 2022-04-22 15:47:42
+ * @LastEditors: Heyuwei
+ * @LastEditTime: 2022-04-25 10:52:01
+ * @FilePath: \SQP_c\include\function.h
+ * @Description: 函数相关操作头文件
+ *
+ * Copyright (c) 2022 by Heyuwei, All Rights Reserved.
+ */
 #include "vector.h"
 #include "matrix.h"
 #pragma once
@@ -7,9 +17,41 @@ struct NdsclaFunction
     double (*function)(Vector *);
 };
 typedef struct NdsclaFunction NdsclaFunction;
+
+/**
+ * @description: 申请一个n维常量值函数
+ * @param {double (*)(Vector *)} 指向一个输入Vector指针,输出Double类型的函数
+ * @param {int} inputsize 函数的输入大小
+ * @return {NdsclaFunction*} 指向一个NdsclaFunction对象的指针
+ */
 NdsclaFunction *ndscla_function_alloc(double (*function)(Vector *), int inputsize);
+
+/**
+ * @description:  调用Ndsclafunction
+ * @param {NdsclaFunction} *function 函数对象
+ * @param {Vector} *x x值
+ * @return {double} 函数调用值
+ */
 double ndscla_function_call(const NdsclaFunction *function, Vector *x);
+
+/**
+ * @description: 用中心梯度法求解Ndsclafunction在x0点的梯度
+ * @param {NdsclaFunction} *function
+ * @param {double} h 步长
+ * @param {Vector} *x0 x0点坐标
+ * @param {Vector} *grad 求出梯度的存储空间
+ * @return {*}
+ */
 void ndscla_central_grad(const NdsclaFunction *function, double h, const Vector *x0, Vector *grad);
+
+/**
+ * @description:  利用中心梯度法求解function的Hession矩阵
+ * @param {NdsclaFunction} *function
+ * @param {double} h 步长
+ * @param {Vector} *x0 x0点坐标
+ * @param {Matrix} *hession hession矩阵
+ * @return {*}
+ */
 void ndscla_central_hession(const NdsclaFunction *function, double h, const Vector *x0, Matrix *hession);
 
 struct vectorfunction
@@ -19,8 +61,45 @@ struct vectorfunction
     void (*function)(const Vector *, Vector *);
 };
 typedef struct vectorfunction NdVectorfunction;
+
+/**
+ * @description: 申请一个n维向量值函数
+ * @param {void (*function)(const Vector *, Vector *)} 一个指向 具有两个Vector指针输入的函数的函数指针,其中左侧vector.size = inputdim,右侧vector.size = outputdim
+ * @param {int} intputdim 输入维度
+ * @param {int} outputdim 输出维度
+ * @return {*}
+ */
 NdVectorfunction *ndVectorfunction_alloc(void (*function)(const Vector *, Vector *), int intputdim, int outputdim);
+
+/**
+ * @description: 释放一个n维向量值函数
+ * @param {NdVectorfunction} *function
+ * @return {*}
+ */
 void ndVectorfunction_free(NdVectorfunction *function);
+
+/**
+ * @description: 调用n维向量值函数
+ * @param {NdVectorfunction} *function 函数对象
+ * @param {Vector} *input input.size = function.inputdim
+ * @param {Vector} *output output.size = function.outputdim
+ * @return {*}
+ */
 void ndVectorfunction_call(const NdVectorfunction *function, const Vector *input, Vector *output);
+
+/**
+ * @description: 求雅克比矩阵
+ * @param {NdVectorfunction} *function
+ * @param {Vector} *x0
+ * @param {double} h
+ * @param {Matrix} *jacobian
+ * @return {*}
+ */
 void ndVectorfunction_jacobian(const NdVectorfunction *function, const Vector *x0, double h, Matrix *jacobian);
+
+/**
+ * @description: 释放n维向量值函数存储空间
+ * @param {NdsclaFunction} *function
+ * @return {*}
+ */
 void ndscla_function_free(NdsclaFunction *function);
--- include/index_set.h
+++ include/index_set.h
@@ -1,3 +1,13 @@
+/*
+ * @Author: HeYuwei
+ * @Date: 2022-03-13 12:36:15
+ * @LastEditors: Heyuwei
+ * @LastEditTime: 2022-04-25 11:00:12
+ * @FilePath: \SQP_c\include\index_set.h
+ * @Description: 指标集头文件
+ *
+ * Copyright (c) 2022 by Heyuwei, All Rights Reserved.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "util.h"
@@ -8,12 +18,73 @@ struct index_set
     int *elem;
 };
 typedef struct index_set Index_set;
+
+/**
+ * @description:申请指标集存储空间
+ * @param {int} size_指标集所能容纳的指标范围   0,1,2,...,size-1;
+ * @return {*} 指向已申请指标集的指针
+ */
 Index_set *index_set_alloc(int size);
+
+/**
+ * @description:释放指针所指向指标集的存储空间
+ * @param {Index_set} *set
+ * @return {*}
+ */
 void index_set_free(Index_set *set);
+
+/**
+ * @description: 向指标集中添加指标
+ * @param {Index_set} *set 指标集
+ * @param {int} i 指标 0 <=i<= index_set.size-1
+ * @return {*}
+ */
 void index_set_append(Index_set *set, const int i);
+
+/**
+ * @description: 在指标集中移除指标
+ * @param {Index_set} *set 指标集
+ * @param {int} i 指标 0 <=i<= index_set.size-1
+ * @return {*}
+ */
 void index_set_remove(Index_set *set, const int i);
+
+/**
+ * @description: 判断指标i是否在指标集中
+ * @param {Index_set} *set 指标集
+ * @param {int} i 指标 0 <=i<= index_set.size-1
+ * @return {*} true/false
+ */
 int index_set_is_in(const Index_set *set, int i);
+
+/**
+ * @description: 指标集求并集
+ * @param {Index_set} *A
+ * @param {Index_set} *B
+ * @param {Index_set} *A_U_B
+ * @return {*}
+ */
 void index_set_union(Index_set *A, Index_set *B, Index_set *A_U_B);
+
+/**
+ * @description: 指标集求交集
+ * @param {Index_set} *A
+ * @param {Index_set} *B
+ * @param {Index_set} *A_I_B
+ * @return {*}
+ */
 void index_set_intersection(const Index_set *A, const Index_set *B, Index_set *A_I_B);
+
+/**
+ * @description: 打印指标集
+ * @param {Index_set} *set
+ * @return {*}
+ */
 void index_set_print(Index_set *set);
+
+/**
+ * @description: 返回指标集当前元素的数量 !!:注意不是指标集的范围
+ * @param {Index_set} *set
+ * @return {*}
+ */
 int index_set_size(const Index_set *set);
